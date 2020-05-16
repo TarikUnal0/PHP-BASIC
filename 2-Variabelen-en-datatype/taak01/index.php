79c64b1105dc9972a75ba686a5772d456787fa5a
Tarik Ünal <2108287@talnet.nl>
2020-05-16 14:01:34 +0200

    Taak 1,2 en 3

---
+++ 2-Variabelen-en-datatype/taak01/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$_score_ = 0;
+
+echo $_score_;
+?>
+</body>
+</html>
---
+++ 2-Variabelen-en-datatype/taak03/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$leeftijd = 16;
+$melk = "1.50";
+echo $leeftijd;
+echo "<br>";
+echo $melk;
+?>
+</body>
+</html>
