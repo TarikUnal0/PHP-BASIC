1e914768e84ca96fc0ab7a90fb637bdc1ec4d6e9
Tarik Ünal <2108287@talnet.nl>
2020-05-18 16:04:01 +0200

    taak 6 7 en 8 (nog bezig)

---
+++ 2-Variabelen-en-datatype/taak06/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$string = "Dit is een string";
+$integer = 2020;
+$float = 14.46;
+$boolean = true;
+$null = null;
+
+ echo gettype($string);
+ echo " ";
+ echo gettype($integer);
+ echo " ";
+ echo gettype($float);
+ echo " ";
+ echo gettype($boolean);
+ echo " ";
+ echo gettype($null);
+?>
+</body>
+</html>
---
+++ 2-Variabelen-en-datatype/taak08/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$voornaam1 = "Tarik";
+$voornaam2 = "Jan";
+$voornaam3 = "Piet";
+
+ echo ;
+?>
+</body>
+</html>
