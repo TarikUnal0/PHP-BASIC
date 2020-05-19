6cd307b819972f7e11c87b5b8a624cc23b59ad9d
Tarik Ünal <2108287@talnet.nl>
2020-05-19 11:12:48 +0200

    taak 8

--- 2-Variabelen-en-datatype/taak08/index.php
+++ 2-Variabelen-en-datatype/taak08/index.php
@@ -6,12 +6,14 @@
     <title>Document</title>
 </head>
 <body>
+<ul>
 <?php
 $voornaam1 = "Tarik";
 $voornaam2 = "Jan";
 $voornaam3 = "Piet";
 
- echo ;
+ echo "<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>" ;
 ?>
+</ul>
 </body>
 </html>
