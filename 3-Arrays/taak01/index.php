debeb83b11b855c1eaa0f58965607591d5345576
Tarik Ünal <2108287@talnet.nl>
2020-05-20 18:29:28 +0200

    Arrays taak 1 en 2

---
+++ 3-Arrays/taak01/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<ul>
+<?php
+$myMovies = ["<li>John Wick</li>", "<li>Avengers</li>", "<li>El Camino</li>"];
+
+echo $myMovies[0];
+echo $myMovies[1];
+echo $myMovies[2];
+?>
+</ul>
+</body>
+</html>
---
+++ 3-Arrays/taak02/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$myDogNames = ["Jip", "Snoepie", "Tom"];
+$myDogNames[1] = "Pim";
+
+echo $myDogNames[0];
+echo $myDogNames[1];
+echo $myDogNames[2];
+
+
+?>
+</body>
+</html>
